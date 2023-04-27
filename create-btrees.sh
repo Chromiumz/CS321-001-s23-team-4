b0ba899caeb2e331931f3406a765493492fcfbda
BoiseState Student <student@u.boisestate.edu>
2023-04-26 19:33:25 -0600

    updated create-btree script

--- create-btrees.sh
+++ create-btrees.sh
@@ -7,7 +7,5 @@ esac
 datafile=$1
 for i in 1 2 3 4 5 6 7 8 9 10 20 31
 do
-	time java -jar build/libs/GeneBankCreateBTree.jar --cache=1 --degree=0 --gbkfile=data/files_gbk/$datafile --length=$i --cachesize=5000 --debug=1
-	mv dump $datafile.dump.$i
+    time java -jar build/libs/GeneBankCreateBTree.jar --cache=1 --degree=0 --gbkfile=$datafile --length=$i --cachesize=5000 --debug=1
 done
-
